4f21e46460b1a6fa545852c31180e63ec2732e44
Lety <taej0127@gmail.com>
2022-05-12 11:43:22 +0900

    chapter5

--- lib/naver_page.dart
+++ lib/naver_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -11,14 +13,40 @@ class NaverPage extends StatefulWidget {
 class _NaverPageState extends State<NaverPage> {
   late WebViewController wvController;
 
+  bool tryLogin = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Naver'),
+      ),
       body: WebView(
-        initialUrl: 'https://google.com',
+        initialUrl: 'https://nid.naver.com/',
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (wvController) {
           this.wvController = wvController;
+        },
+        onPageFinished: (page) async {
+          log('$page is loaded');
+
+          if (tryLogin) {
+            log('로그인에 실패 했습니다.');
+          } else {
+            setState(() {tryLogin = true;});
+
+            var userId = "id";
+            var userPw = "password";
+
+            wvController
+                .runJavascript('document.getElementById("id").value="$userId"');
+            wvController
+                .runJavascript('document.getElementById("pw").value="$userPw"');
+
+            await Future.delayed(const Duration(milliseconds: 500));
+
+            await wvController.runJavascript('document.forms[0].submit()');
+          }
         },
       ),
     );
